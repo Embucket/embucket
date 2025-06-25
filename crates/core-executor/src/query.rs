9f692a2ab108cbf11612be412b827e1a02155ac7
Jan Kaul <jan@embucket.com>
2025-06-25 08:22:15 +0200

    add source_exists to merge projection

--- crates/core-executor/src/query.rs
+++ crates/core-executor/src/query.rs
@@ -2055,7 +2055,7 @@ pub fn merge_clause_projection<S: ContextProvider>(
             _ => (),
         }
     }
-    let exprs = target_schema
+    let mut exprs: Vec<datafusion_expr::Expr> = target_schema
         .iter()
         .map(|(table_reference, field)| {
             let name = table_reference
@@ -2085,6 +2085,7 @@ pub fn merge_clause_projection<S: ContextProvider>(
         })
         .collect::<Result<_, DataFusionError>>()
         .context(ex_error::DataFusionSnafu)?;
+    exprs.push(col(SOURCE_EXISTS));
     Ok(exprs)
 }
 
