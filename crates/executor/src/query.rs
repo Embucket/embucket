bd2b7c9035a96343341ce64bd0eb4a3d27ed427a
DanCodedThis <94703934+DanCodedThis@users.noreply.github.com>
2025-11-13 14:06:37 +0200

    incorrect status message fix (#11)

--- crates/executor/src/query.rs
+++ crates/executor/src/query.rs
@@ -29,7 +29,7 @@ use catalog_metastore::{
     TableFormat as MetastoreTableFormat, TableIdent as MetastoreTableIdent, Volume, VolumeType,
     models::volumes::create_object_store_from_url,
 };
-use datafusion::arrow::array::{Int64Array, RecordBatch};
+use datafusion::arrow::array::{Int64Array, RecordBatch, StringArray};
 use datafusion::arrow::datatypes::{DataType, Field, Schema as ArrowSchema, SchemaRef};
 use datafusion::arrow::datatypes::{Fields, SchemaBuilder};
 use datafusion::catalog::{CatalogProvider, SchemaProvider};
@@ -2766,7 +2766,15 @@ impl UserQuery {
             false,
         )]));
         Ok(QueryResult::new(
-            vec![RecordBatch::new_empty(schema.clone())],
+            vec![
+                RecordBatch::try_new(
+                    schema.clone(),
+                    vec![Arc::new(StringArray::from(vec![
+                        "Statement executed successfully.",
+                    ]))],
+                )
+                .context(ex_error::ArrowSnafu)?,
+            ],
             schema,
             self.query_context.query_id,
         ))
