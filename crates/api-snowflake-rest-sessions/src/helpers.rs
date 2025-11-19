6f452dd549c165119efc839fe0fcedf552dbcc06
DanCodedThis <94703934+DanCodedThis@users.noreply.github.com>
2025-11-19 13:10:23 +0200

    sessions: JWT (#19)
    * jwt token sessions
    
    # Conflicts:
    #	crates/embucket-lambda/src/main.rs
    
    * Requested changes
    
    * clippy
    
    * fmt
    
    * clippy

---
+++ crates/api-snowflake-rest-sessions/src/helpers.rs
@@ -0,0 +1,62 @@
+use chrono::offset::Local;
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation, decode, encode};
+use serde::{Deserialize, Serialize};
+use time::Duration;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(test, derive(Debug))]
+pub struct Claims {
+    pub sub: String, // token issued to a particular user
+    pub iat: i64,    // Issued At
+    pub exp: i64,    // Expiration Time
+    pub session_id: String,
+}
+
+#[must_use]
+pub fn jwt_claims(username: &str, expiration: Duration) -> Claims {
+    let now = Local::now();
+    let iat = now.timestamp();
+    let exp = now.timestamp() + expiration.whole_seconds();
+
+    Claims {
+        sub: username.to_string(),
+        iat,
+        exp,
+        session_id: Uuid::new_v4().to_string(),
+    }
+}
+
+pub fn get_claims_validate_jwt_token(
+    token: &str,
+    jwt_secret: &str,
+) -> Result<Claims, jsonwebtoken::errors::Error> {
+    let mut validation = Validation::default();
+    validation.leeway = 5;
+    validation.set_required_spec_claims(&["exp"]);
+
+    let decoding_key = DecodingKey::from_secret(jwt_secret.as_bytes());
+
+    let decoded = decode::<Claims>(token, &decoding_key, &validation)?;
+
+    Ok(decoded.claims)
+}
+
+pub fn create_jwt<T>(claims: &T, jwt_secret: &str) -> Result<String, jsonwebtoken::errors::Error>
+where
+    T: Serialize,
+{
+    encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(jwt_secret.as_bytes()),
+    )
+}
+
+#[must_use]
+pub fn ensure_jwt_secret_is_valid(jwt_secret: &str) -> Option<String> {
+    if jwt_secret.is_empty() {
+        return None;
+    }
+    Some(jwt_secret.to_string())
+}
