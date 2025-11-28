573ec5bee12b15d087e258499c082f24d0682247
Yaroslav Litvinov <yaroslav@embucket.com>
2025-11-28 18:51:12 +0100

    clean unused code / features

--- crates/executor/src/utils.rs
+++ crates/executor/src/utils.rs
@@ -29,8 +29,6 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use strum::{Display, EnumString};
 
-pub static DEFAULT_QUERY_HISTORY_ROWS_LIMIT: usize = 50;
-
 #[derive(Clone, Debug)]
 pub struct Config {
     pub embucket_version: String,
@@ -43,7 +41,6 @@ pub struct Config {
     pub mem_pool_size_mb: Option<usize>,
     pub mem_enable_track_consumers_pool: Option<bool>,
     pub disk_pool_size_mb: Option<usize>,
-    pub query_history_rows_limit: usize,
     pub read_only: bool,
     pub max_concurrent_table_fetches: usize,
 }
@@ -68,7 +65,6 @@ impl Default for Config {
             mem_pool_size_mb: None,
             mem_enable_track_consumers_pool: None,
             disk_pool_size_mb: None,
-            query_history_rows_limit: DEFAULT_QUERY_HISTORY_ROWS_LIMIT,
             read_only: false,
             max_concurrent_table_fetches: 5,
         }
@@ -94,12 +90,6 @@ impl Config {
         bootstrap_default_entities: bool,
     ) -> Self {
         self.bootstrap_default_entities = bootstrap_default_entities;
-        self
-    }
-
-    #[must_use]
-    pub const fn with_query_history_rows_limit(mut self, limit: usize) -> Self {
-        self.query_history_rows_limit = limit;
         self
     }
 
