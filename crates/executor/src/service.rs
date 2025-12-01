967a73160c1f0eeced28b188eaf0c7b55923523a
DanCodedThis <daniil.vysotskyi@gmail.com>
2025-12-01 13:19:17 +0200

    bootstrap remove clippy + fmt

--- crates/executor/src/service.rs
+++ crates/executor/src/service.rs
@@ -31,11 +31,8 @@ use crate::running_queries::RunningQueryId;
 use crate::session::{SESSION_INACTIVITY_EXPIRATION_SECONDS, to_unix};
 use crate::tracing::SpanTracer;
 use crate::utils::{Config, MemPoolType};
-use catalog::catalog_list::{DEFAULT_CATALOG, EmbucketCatalogList};
-use catalog_metastore::{
-    Database, InMemoryMetastore, Metastore, Schema, SchemaIdent, TableIdent as MetastoreTableIdent,
-    Volume, VolumeType,
-};
+use catalog::catalog_list::EmbucketCatalogList;
+use catalog_metastore::{InMemoryMetastore, Metastore, TableIdent as MetastoreTableIdent};
 use tokio::sync::RwLock;
 use tokio::time::Duration;
 use tracing::Instrument;
