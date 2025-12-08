601db9f672db5b54722d3fec41332134c49f13f7
osipovartem <artemosipov.dev@gmail.com>
2025-12-08 16:50:48 +0300

    merge

--- crates/executor/src/query.rs
+++ crates/executor/src/query.rs
@@ -1562,6 +1562,7 @@ impl UserQuery {
         };
 
         let catalog = self.get_catalog(&schema_ref.catalog)?;
+        println!("dddd {:?}", catalog.schema(&schema_ref.schema));
         if catalog.schema(&schema_ref.schema).is_some() {
             if if_not_exists {
                 return self.created_entity_response();
