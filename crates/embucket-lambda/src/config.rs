ac6ba62b2243d93543b041d7848a45abf1f2e565
DanCodedThis <daniil.vysotskyi@gmail.com>
2025-12-15 21:37:40 +0200

    fixes for honeycomb

--- crates/embucket-lambda/src/config.rs
+++ crates/embucket-lambda/src/config.rs
@@ -64,7 +64,7 @@ impl EnvConfig {
                 .unwrap_or(3),
             otel_grpc: parse_env("OTEL_GRPC").unwrap_or(true),
             tracing_level: env_or_default("TRACING_LEVEL", "INFO"),
-            log_format: env_or_default("LOG_FORMAT", "pretty"),
+            log_format: env_or_default("LOG_FORMAT", "json"),
             log_filter: parse_env("RUST_LOG").unwrap_or_else(|| "INFO".to_string()),
         }
     }
